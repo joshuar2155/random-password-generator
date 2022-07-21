3957104a48f675be9213273c60815cba7733a246
Josh Rae <joshuar2155@gmail.com>
2022-07-21 18:07:54 -0400

    password function sample

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -18,3 +18,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+
+function generatePassword() {
+  var length = 8,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
