1c2d20ea70476e8eb0efbd56e1b0c1c614a6b14e
Josh Rae <joshuar2155@gmail.com>
2022-07-21 19:40:23 -0400

    somethings works

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -23,103 +23,19 @@ var playGame = function() {
 
 function generatePassword() {
   var password = "password";
-  var passLength = 0;
-  var useUpper = true;
-  var useLower = true;
-  var useSpecial = true;
-  var useNumber = true;
-  var lowerChars = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  var upperChars = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-  var numChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var specChars = ["!", "#", "$", "%", "&", "*", "'"];
-  var choices = [];
+  var lowercase = true;
+  var uppercase = true;
+  var numbers = true;
+  var special = true;
+  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var special = ["*","&", "^", "%", "$", "#", "@", "!"];
+  var numbers = [0,1,2,3,4,5,6,7,8,9];
+  var passwordnumbers = [8-128];
 
-  // TODO: add code to generate the password here
-
-  passLength = prompt(
-    "how many characters would you like your password to be? Can be 8-128 chars"
-  );
-  useUpper = confirm(
-    "Would you like your password to contain uppercase letters?"
-  );
-  useLower = confirm(
-    "Would you like your password to contain lowercase letters?"
-  );
-  useSpecial = confirm(
-    "Would you like your password to contain special characters?"
-  );
-  useNumber = confirm("Would you like your password to contain numbers?");
-
-  if (useUpper === true) {
-    choices = choices.concat(upperChars);
-  }
-
-  if (useLower === true) {
-    choices= choices.concat(lowerChars);
-  }
-
-  if (useNumber === true) {
-    choices = choices.concat(numChars);
-  }
-
-  if (useSpecial === true) {
-    choices = choices.concat(specChars);
-  }
+  passwordnumbers = window.prompt("How long would you like your password?");
 }
+
 
 // Write password to the #password input
 function writePassword() {
